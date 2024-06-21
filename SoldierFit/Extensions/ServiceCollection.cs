922d450d4d32cafff92418281676b272832e113c
milewSasho <mileww.sasho@abv.bg>
2024-06-21 22:16:28 +0300

    Change password requirements for seed test users

--- SoldierFit/Extensions/ServiceCollection.cs
+++ SoldierFit/Extensions/ServiceCollection.cs
@@ -54,7 +54,11 @@
             services
                 .AddDefaultIdentity<IdentityUser>(options => 
                 {
-                    options.SignIn.RequireConfirmedAccount = true;
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
                 })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
