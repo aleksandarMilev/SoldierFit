549c7dcffcf18b08f3631bde3897eaacc280d2cc
milewSasho <mileww.sasho@abv.bg>
2024-06-22 12:34:23 +0300

    Create WorkoutController and implment index action and view

---
+++ SoldierFit/Controllers/WorkoutController.cs
@@ -0,0 +1,32 @@
+﻿namespace SoldierFit.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SoldierFit.Core.Contracts;
+    using SoldierFit.Core.Models;
+
+    public class WorkoutController : Controller
+    {
+        private readonly IWorkoutService service;
+
+        public WorkoutController(IWorkoutService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<WorkoutDto> models;
+
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                models = await service.AllWorkoutsAsync();
+            }
+            else
+            {
+                models = await service.LastThreeWorkoutsAsync();
+            }
+
+            return View(models);
+        }
+    }
+}
